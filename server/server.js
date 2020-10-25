8904baf116c024278d6f12f301bdba9747073776
vidhur2k <vidhur2k@gmail.com>
2020-10-25 12:56:11 -0400

    Added mongodb atlas db connection using mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+const mongoURL = 'mongodb+srv://vidhur2k:vidhur12345@eplgames.t3whn.mongodb.net/eplgames'
+mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get('/', (req, res) => {
+    console.log(mongoose.connection);
     res.send('hello');
 });
 
