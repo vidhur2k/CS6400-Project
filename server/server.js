c09ac9209b58d0e6a0defe22f56493a7891c8607
vidhur2k <vidhur2k@gmail.com>
2020-10-25 12:23:20 -0400

    Added server template

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on Port 3000...')
+});
